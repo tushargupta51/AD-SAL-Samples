ab85509cfd1dbb6145d9ccedbfb691a6b57323b4
unknown <tugupta@tugupta-2013VM.redmond.corp.microsoft.com>
2015-07-23 13:47:32 -0700

    Adding web application project - sign in user

---
+++ WebApplication/WebApplication/App_Start/Startup.Auth.cs
@@ -0,0 +1,46 @@
+﻿using Owin;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+
+namespace WebApplication
+{
+    public partial class Startup
+    {
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            // Enable the application to use a cookie to store information for the signed in user
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+                {
+                    Authority = "https://login.microsoftonline.com/tushartest.onmicrosoft.com/",
+                    ClientId = "ba7651c2-53c2-442a-97c2-3d60ea42f403",
+                    RedirectUri = "http://localhost:42023"
+                });
+
+            // Use a cookie to temporarily store information about a user logging in with a third party login provider
+            //app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Uncomment the following lines to enable logging in with third party login providers
+            //app.UseMicrosoftAccountAuthentication(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //app.UseTwitterAuthentication(
+            //   consumerKey: "",
+            //   consumerSecret: "");
+
+            //app.UseFacebookAuthentication(
+            //   appId: "",
+            //   appSecret: "");
+
+            //app.UseGoogleAuthentication();
+        }
+    }
+}
---
+++ WebApplication/WebApplication/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.Owin.Security.Cookies;
+
+namespace WebApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/Login
+        public ActionResult Login(string returnUrl)
+        {
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(new AuthenticationProperties { RedirectUri = "/" }, OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+            return View();
+        }
+
+        public void LogOff()
+        {
+            // Send an OpenID Connect sign-out request.
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                OpenIdConnectAuthenticationDefaults.AuthenticationType, CookieAuthenticationDefaults.AuthenticationType);
+        }
+    }
+}
